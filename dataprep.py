825ba9cce2eb54af4dfe973a22acccabb1b2f963
Azest-japan <56631844+Azest-japan@users.noreply.github.com>
2019-11-26 17:56:35 +0900

    Update dataprep.py

--- dataprep.py
+++ dataprep.py
@@ -6,7 +6,7 @@ Created on Tue Oct  8 17:28:45 2019
 """
 
 
-import skimage.io as io
+
 import sys
 import os
 import cv2
@@ -37,11 +37,13 @@ imgdata = None
 def loadimg(fpath,ftype):
     
     global sample
-    imgdict = {}
+    imgdict = {}       # Dictionary - key = Patient Number(String) Value = Patient Images(List)
+    
+    #Load Dicom Images
     if ftype == 'dicom':
         for path,subdir,files in os.walk(fpath):
-            name = os.path.basename(path)
-            imglist = []
+            name = os.path.basename(path)  # Patient ID as Key
+            imglist = []                   # List of Patient Images
             for file in files:
                 full_path = path+ '\\' + file
                 if int(file.replace('Image',''))%2 != 0:
@@ -49,28 +51,28 @@ def loadimg(fpath,ftype):
                     if sample == None:
                         sample = imgdata
                     img = imgdata.pixel_array
-                    imglist.append(img[40:-40,:,:])
+                    imglist.append(img[40:-40,:,:]) # cut the top and bottom borders (40 px)
                     
             if len(imglist) != 0:
-                imgdict[name] = imglist
-                
+                imgdict[name] = imglist   # Dict[key] = List
+    
+    #Load raw Jpeg Images
     elif ftype == 'jpg':
         for path,subdir,files in os.walk(fpath):
-            name = os.path.basename(path)
-            imglist = []
+            name = os.path.basename(path)  # Patient ID as Key
+            imglist = []                   # List of Patient Images
             for file in files:
                 full_path = path+ '\\' + file
-                
                 if '.jpg' in full_path and 'red' in full_path:
                     img = cv2.imread(full_path)
-                    imglist.append(img[40:-40,:,:])
+                    imglist.append(img[40:-40,:,:]) # cut the top and bottom borders (40 px)
                 
             if len(imglist) != 0:
-                imgdict[name] = imglist
+                imgdict[name] = imglist   # Dict[key] = List
                 
+    #Load Annotated Images
     elif ftype == 'annotation':
-        print('annotation')
-        m = '01'
+        m = '01'                          # Patient ID as Key
         for path,subdir,files in os.walk(fpath):
             for file in files:
                 full_path = path+'\\'+file
@@ -85,14 +87,14 @@ def loadimg(fpath,ftype):
             
     return imgdict
 
-imgd = loadimg(dicom_path,'dicom')
+#imgd = loadimg(dicom_path,'dicom')
 
 clr = np.random.rand(8,3)*255
 maskcolor = dict((i+1,clr[i]) for i in range(8))
 
 def load_model2():
-    model = load_model('C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\mymodel.h5')
-    model.load_weights('C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\best_weights.hdf5')
+    model = load_model('/home/barath/Downloads/home/barath/Downloads/mymodel.h5')
+    model.load_weights('/home/barath/Downloads/home/barath/Downloads/best_weights.hdf5')
     opt = keras.optimizers.Adam(lr=0.0002, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
     model.compile(loss='categorical_crossentropy',
                   optimizer=opt,
@@ -100,25 +102,7 @@ def load_model2():
     return model
 #imgj = loadimg(jpg_path,'jpg')
 
-# resizes image while maintaining aspect ratio
-def img_resize(img,final_shape):
-    
-    m,n = img.shape
-    mx = max(m,n)
-    ratio = final_shape/mx
-    #print(ratio)
-    rimg = cv2.resize(img,dsize=None,fx=ratio,fy=ratio)
-    ax = 0
-    i1 = np.uint8(np.zeros((final_shape-min(rimg.shape),final_shape)))
-    #print(i1.shape)
-    if m>n:
-        i1 = i1.T
-        ax = 1
-    rimg = np.uint8(np.concatenate([i1,rimg],axis=ax))
-    
-    assert rimg.shape == (final_shape,final_shape)
-    return rimg
-    #print(rimg.shape)
+
 
 #segments the image based on the input points
 def segment(img):
@@ -180,63 +164,20 @@ def segment(img):
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
     
-
-def cut_alt(img):
-    e1 = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=3)
-    e1 = np.uint8(cv2.cvtColor(e1,cv2.COLOR_BGR2GRAY))
-#    e1[e1>=100] = 255
-#    e1[e1<100] = 0
+# Cut out the relevant portion of image
+def cut(img):  # image has dimension (length,breadth,3) 3 = BGR
     
-    e = np.where(e1.sum(axis=1)/(255*e1.shape[1])>0.3)[0]
-    top = e[0]
-    tot = 0
-    print(e)
-    for i in range(e[0],e[0]+10):
-        if i in e:
-            tot += 1
-        else:
-            tot = 0
-        if tot >= 7:
-            top = i
-    #print(e)
-    ec = e1.sum(axis=0)/(255*e1.shape[0])
-    print(np.round(ec*100))
-    i = 0
-    left = False
-    right = False
-    li = int(e1.shape[1]/2)
-    ri = int(e1.shape[1]/2)
-    for i in range(int(e1.shape[1]/2)):
-        if left == False:
-            li = int(e1.shape[1]/2-i)
-            if ec[li]<0.1:
-                left = True
-        if right == False:
-            ri = int(e1.shape[1]/2+i)
-            if ec[ri]<0.1:
-                right = True
-    print(li,ri)
-    cv2.imwrite('C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\demo.png',e1)
-    
-    cv2.imshow('img',img[top:,li:ri])
-    cv2.imshow('img2',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-
-
-def cut(img):
-
+    # Sum the color modes, B+G+R, resulting dimension (length,breadth,1) 
     mono_img = np.sum(img, axis=2)
-    #bin_img = np.sign(np.where((mono_img>140)&(mono_img<170), 0, mono_img))
     
     row_activate = np.zeros(mono_img.shape[0])
     col_activate = np.zeros(mono_img.shape[1])
     
     for row in range(mono_img.shape[0]):
+        #Count the number of unique values in each row
         row_activate[row] = len(np.unique(mono_img[row]))
     for col in range(mono_img.shape[1]):
+        #Count the number of unique values in each column
         col_activate[col] = len(np.unique(mono_img[:,col]))
     
     judge_len = 30
@@ -246,6 +187,7 @@ def cut(img):
     
     top = 0
     bottom = mono_img.shape[0]-1
+    
     for t in range(mono_img.shape[0]-judge_len):
         if all(row_activate[t:t+judge_len] >= min_unique_1):
             top = t
@@ -276,23 +218,25 @@ def cut(img):
     return top, bottom, left, right
 
 
-
+# Extract the column corresponding to the scale 
 def scale(img,top,bottom,left):
     i2 = img.copy()
     
+    # convert BGR image to gray scale
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     mask = cv2.inRange(img,160,255)
-    img[mask==0]=0
-    mx = np.max(img[:,:left])
-    minmax = np.min([200,mx])
-    print(top,bottom,left,mx,img.shape)
+    img[mask==0]=0  # All pixels below 160 becomes 0
+    mx = np.max(img[:,:left])  # max pixel value 
+    minmax = np.min([200,mx])  
+    #print(top,bottom,left,mx,img.shape)
+    
     
     j = top - 10
-    s = j
+    s = j  
     length = 0
     repetition = 0
     col = []
-    ll = []
+    ll = []  # stores the pixel length between subsequent points in a column
     row = 0
     length_prev = 0
     for i in range(left):
@@ -329,13 +273,13 @@ def scale(img,top,bottom,left):
                     if repetition >= 3:
                         ll.append((j,length,length_prev))
                         print('* ',i,j,s,s-length_prev)
-                        row = j
                         col.append(i)
                         repetition = 0
                         print('')
                         break
                     s = j
                     j += 15
+                    
                 elif length!= 0 and ((j-s)<=0.9*length or (j-s)>=1.1*length):
                     #print('a__3',i,j,s,length)
                     length = j-s
@@ -346,9 +290,11 @@ def scale(img,top,bottom,left):
             break
         
     print(col,ll)
+    
     if len(col)!=0:
-        cv2.imshow('scale',np.concatenate((np.concatenate((np.zeros([row+15,100]),img[0:row+15,col[-1]:col[-1]+1]),axis=1),np.zeros([row+15,100])),axis=1))
+        cv2.imshow('scale',np.concatenate((np.concatenate((np.zeros([ll[0][0]+30-max(top-10,0),100]),img[max(top-10,0):ll[0][0]+30,col[-1]:col[-1]+1]),axis=1),np.zeros([ll[0][0]+30-max(top-10,0),100])),axis=1))
         #cv2.imwrite('C:\\Users\\AZEST-2019-07\\Desktop\\pyfiles\\scale.png',cv2.resize(img[0:row+15,col-32:col+32], dsize=(128,2*(row+15))))
+    
     i2[:,col] = [0,100,255]
     cv2.imshow('org',i2[max(top-10,0):ll[0][0]+30,:left])
     cv2.waitKey(0)
@@ -356,136 +302,9 @@ def scale(img,top,bottom,left):
     return col[0],ll[0]
 
 
-def create_image(imgd,img,color,k,index,c=5):
-    top,bottom,left,right = cut(imgd[k][min(int(index/2),1,c)])          
-    img = img[top:bottom,left:right]
-    img2 = np.zeros(img.shape)
-    r,c,d = img.shape
-    #print(r,c,k,index,index/2)
-    for i in range(r):
-        for j in range(c):
-            if (img[i,j]>[0,30,100]).sum()>=3 and (img[i,j]<[80,160,256]).sum()>=3:
-                img2[i,j] = np.array(color)
-    cv2.imwrite('D:\\Ito data\\annotated\\'+k+str(index)+'.png',img2)
-#    cv2.imshow('img',img2)
-#    cv2.waitKey(0)
-#    cv2.destroyAllWindows()
-#    return img2
-    
-def create_map(imgd,imgj):
-    for k,v in imgj.items():
-        for i in range(len(v)):
-            if i%2==0:
-                color = [0,255,0]
-            else:
-                color = [0,255,255]
-            create_image(imgd,v[i],color,k,i)
-
-
-#create_map(imgd,imgj)
-#create_image(imgd,imgj['05'][2],[0,255,255],'05',2,0)
-#create_image(imgd,imgj['05'][3],[0,255,0],'05',3)
-#create_image(imgd,imgj['05'][4],[0,255,255],'05',4)
-
-
-
-def add(i1,i2,k,index):
-    r0,c0,d0 = i1.shape
-    r1,c1,d1 = i2.shape
-    
-    r,c = min(r0,r1),min(c0,c1)
-    #print(r,c)
-    
-    i1 = i1[0:r,0:c,:]
-    i2 = i2[0:r,0:c,:]
-    
-    i3 = cv2.add(i1,i2)
-    cv2.imwrite('D:\\Ito data\\overlap\\'+k+str(index)+'.png',i3)
-#    cv2.imshow('i1',i1)
-#    cv2.imshow('i2',i2)
-#    cv2.imshow('i3',i3)
-#    cv2.waitKey(0)
-#    cv2.destroyAllWindows()
-    
-    
-def overlap(imgA):
-    for k,v in imgA.items():
-        for i in range(2):
-            i1 = v[i*2]
-            i2 = v[i*2+1]
-            add(i1,i2,k,i)
-            
-#imgA = loadimg(annotated_path,'annotation')
-#
-#overlap(imgA)
-#add(imgA['05'][3],imgA['05'][4],'05',1)
-
-def one_hot(overlap_path,fd=6):   # 1-thyroid, 2-papillary, 3-benign, 4-cyst, 5-solid lesions 0-other
-    imglist = []
-    #thyroid green 
-    #outline yellow
-    #papillary red 
-    #benign blue 252,3,22
-    #cyst violet 239,27,218
-    #solid-lesion pale white 180,207,216
-    
-    for path, subdir, files in os.walk(overlap_path):
-        fimg_list = []
-        for file in files:
-            full_path = path + '\\' + file
-            img = cv2.imread(full_path)     
-            #reshape(img)
-            r,c,d = img.shape
-            
-            fimg = np.uint8(np.zeros([r,c]))
-            
-            for i in range(r):
-                for j in range(c):
-                    b,g,r = img[i,j]
-                    
-                    if g>2*r and g > 2*b:            # green thyroid 1
-                        fimg[i,j] = 1
-                        
-                    elif r>2*g and r>2*b:            # red papillary 2
-                        fimg[i,j] = 2 
-                        
-                    elif b<60 and g<60 and r<60:     # black other 0
-                        fimg[i,j] = 0
-                        
-                    elif b>200 and b>3*g and b>3*r:  # blue benign 3 
-                        fimg[i,j] = 3
-                        
-                    elif b>180 and r>180 and g<80:   # violet cyst 4
-                        fimg[i,j] = 4
-                        
-                    elif r>180 and g>180 and b>150:  # pale white solid-lesion 5
-                        fimg[i,j] = 5
-                        
-                    elif r>200 and g>200 and b<60:   # yellow outline 1
-                        fimg[i,j] = 1
-                        
-            fimg = to_categorical(fimg,fd)
-            fimg_list.append(fimg)
-            return fimg_list
-
-model = load_model2()
-#print(model.summary())
-
-num_dict = {}
-for i in range(10):
-    num_dict[i] = i
-    num_dict[10+i] = i+0.5
-
-num_dict[20] = -1
-#img = imgd['03'][1]
-#top,bottom,left,right = cut(img)
-#
-#col,ll = scale(img,top,bottom,left)
-
-
+# Extract the pixels of the scale and measure the distance if there is a number beside it
 def extract(img,col):
    
-    
     i1 = img[max(top-10,0):min(ll[0]+30,bottom),col:col+1]
     mask = cv2.inRange(img,100,255)
     img[mask == 0] = 0
@@ -495,7 +314,6 @@ def extract(img,col):
     peak = []
     m = 0
     s = 0
-    
     for r in range(row):    
         if i1[r,0] >= 140:
             m = r
@@ -507,7 +325,7 @@ def extract(img,col):
     
     #print(peak,i1.shape)
     d = 0
-    m = ''
+    m = ''    # left side(l) or right side(r)
     dist = []
     l0 = 0
     r0 = 0
@@ -537,20 +355,14 @@ def extract(img,col):
                 d = pos
                 r0 = r
             
-        cv2.imshow('l',lside)
-        cv2.imshow('r',rside)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+#        cv2.imshow('l',lside)
+#        cv2.imshow('r',rside)
+#        cv2.waitKey(0)
+#        cv2.destroyAllWindows()
     return peak,dist
-        
-    
-peak,dist = extract(img.copy(),col)
-d_avg = np.average(dist)
-print(d_avg)
-    
-#fhot = one_hot(overlap_path,final_dim)
 
-# resizes image while maintaining aspect ratio
+
+# resizes image based on distance while maintaining aspect ratio
 def img_resize(img,d_avg,final_shape):
     
     m,n = img.shape
@@ -562,6 +374,33 @@ def img_resize(img,d_avg,final_shape):
     
     return img
 
+    
+# cnn model trained on decimal mnist
+model = load_model2()
+
+# maps model output to numbers
+num_dict = {}
+for i in range(10):
+    num_dict[i] = i
+    num_dict[10+i] = i+0.5
+
+num_dict[20] = -1
+
+path = '/home/barath/Downloads/Thyroid/AI2/01/Image009.jpg'
+img = cv2.imread(path)[40:-40,:]
+
+
+top,bottom,left,right = cut(img)
+print(bottom-top,right-left)
+#print(img.shape,top,bottom,left)
+
+
+col,ll = scale(img,top,bottom,left)
+# Convert image to gray scale
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+peak,dist = extract(img.copy(),col)
+d_avg = np.average(dist)
+print(d_avg)
 
 img = img_resize(img[top:bottom,left:right],d_avg,512)
 cv2.imshow('i3',img)
@@ -570,5 +409,119 @@ cv2.destroyAllWindows()
 
 
 
+# extract the outline of the annotated image
+def create_image(imgd,img,color,k,index,c=5):
+    top,bottom,left,right = cut(imgd[k][min(int(index/2),1,c)])          
+    img = img[top:bottom,left:right]
+    img2 = np.zeros(img.shape)
+    r,c,d = img.shape
+    #print(r,c,k,index,index/2)
+    for i in range(r):
+        for j in range(c):
+            if (img[i,j]>[0,30,100]).sum()>=3 and (img[i,j]<[80,160,256]).sum()>=3:
+                img2[i,j] = np.array(color)
+    cv2.imwrite('D:\\Ito data\\annotated\\'+k+str(index)+'.png',img2)
+#    cv2.imshow('img',img2)
+#    cv2.waitKey(0)
+#    cv2.destroyAllWindows()
+#    return img2
 
+# extract the outline of all images by iteration
+def create_map(imgd,imgj):
+    for k,v in imgj.items():
+        for i in range(len(v)):
+            if i%2==0:
+                color = [0,255,0]
+            else:
+                color = [0,255,255]
+            create_image(imgd,v[i],color,k,i)
+
+# add the thyroid and nodule images of a single patient
+def add(i1,i2,k,index):
+    r0,c0,d0 = i1.shape
+    r1,c1,d1 = i2.shape
     
+    r,c = min(r0,r1),min(c0,c1)
+    #print(r,c)
+    
+    i1 = i1[0:r,0:c,:]
+    i2 = i2[0:r,0:c,:]
+    
+    i3 = cv2.add(i1,i2)
+    cv2.imwrite('D:\\Ito data\\overlap\\'+k+str(index)+'.png',i3)
+#    cv2.imshow('i1',i1)
+#    cv2.imshow('i2',i2)
+#    cv2.imshow('i3',i3)
+#    cv2.waitKey(0)
+#    cv2.destroyAllWindows()
+    
+# add the thyroid and nodule images of all patients by iteration
+def overlap(imgA):
+    for k,v in imgA.items():
+        for i in range(2):
+            i1 = v[i*2]
+            i2 = v[i*2+1]
+            add(i1,i2,k,i)
+
+#create_map(imgd,imgj)
+#create_image(imgd,imgj['05'][2],[0,255,255],'05',2,0)
+#create_image(imgd,imgj['05'][3],[0,255,0],'05',3)
+#create_image(imgd,imgj['05'][4],[0,255,255],'05',4)
+
+#imgA = loadimg(annotated_path,'annotation')
+#
+#overlap(imgA)
+#add(imgA['05'][3],imgA['05'][4],'05',1)
+
+# Convert color -> classes and classes -> one-hot vector
+def one_hot(overlap_path,fd=6):   # 1-thyroid, 2-papillary, 3-benign, 4-cyst, 5-solid lesions 0-other
+    #thyroid green 
+    #outline yellow
+    #papillary red 
+    #benign blue 252,3,22
+    #cyst violet 239,27,218
+    #solid-lesion pale white 180,207,216
+    
+    for path, subdir, files in os.walk(overlap_path):
+        fimg_list = []
+        for file in files:
+            full_path = path + '\\' + file
+            img = cv2.imread(full_path)     
+            #reshape(img)
+            r,c,d = img.shape
+            
+            fimg = np.uint8(np.zeros([r,c]))
+            # color -> classes
+            for i in range(r):
+                for j in range(c):
+                    b,g,r = img[i,j]
+                        
+                    if b<60 and g<60 and r<60:       # black other 0
+                        fimg[i,j] = 0
+                    
+                    elif g>2*r and g > 2*b:          # green thyroid 1
+                        fimg[i,j] = 1
+                        
+                    elif r>2*g and r>2*b:            # red papillary 2
+                        fimg[i,j] = 2 
+                        
+                    elif b>200 and b>3*g and b>3*r:  # blue benign 3 
+                        fimg[i,j] = 3
+                        
+                    elif b>180 and r>180 and g<80:   # violet cyst 4
+                        fimg[i,j] = 4
+                        
+                    elif r>180 and g>180 and b>150:  # pale white solid-lesion 5
+                        fimg[i,j] = 5
+                        
+                    elif r>200 and g>200 and b<60:   # yellow outline 1
+                        fimg[i,j] = 1
+            
+            #color -> one-hot vector
+            fimg = to_categorical(fimg,fd)
+            fimg_list.append(fimg)
+            return fimg_list
+
+#fhot = one_hot(overlap_path,final_dim)
+
+
